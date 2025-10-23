cff1eac4b6c4bd2e8b76786b7dfdbf3743dce959
Sean Mooney <sean@seanmooney.info>
2025-10-23 13:07:34 +0000

    Fix URL construction to point to index.html
    - Append 'index.html' to the log URL so it points directly to the directory listing
    - This ensures the URL is accessible in browsers without requiring directory listing
    - Fixes the issue where Zuul links to directory path instead of index.html file

--- roles/upload-logs-minio/library/zuul_s3_upload.py
+++ roles/upload-logs-minio/library/zuul_s3_upload.py
@@ -107,12 +107,12 @@ class Uploader():
         if endpoint:
             self.endpoint = endpoint
             self.url = os.path.join(endpoint,
-                                    bucket, self.prefix)
+                                    bucket, self.prefix, 'index.html')
         else:
             self.endpoint = 'https://s3.amazonaws.com/'
             return_endpoint = 'https://' + bucket + '.s3.amazonaws.com/'
             self.url = os.path.join(return_endpoint,
-                                    self.prefix)
+                                    self.prefix, 'index.html')
 
         # The default for max_pool_connections is 10, which is easily
         # exhausted if the upload threads overwhelm them.
